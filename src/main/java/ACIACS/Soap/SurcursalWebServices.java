5c18b45324b1f24abecc0744519bf452b9097b30
Daniel P. M <danielmoronta23@gmail.com>
2020-11-25 23:28:11 -0400

    Se modifico la funcion de agregarTest del SOAP: - Se agrego el retorno objeto Error. - Se casteo el modulo para evitar errores. - Se corrigio error de conteo de persona dentro de local.

--- src/main/java/ACIACS/Soap/SurcursalWebServices.java
+++ src/main/java/ACIACS/Soap/SurcursalWebServices.java
@@ -24,7 +24,7 @@ public class SurcursalWebServices {
     public Object[] capacidadSucursal(String idSucursal) {
         Object[] resultado = new Object[3];
         Sucursal sucursal = controladora.buscarSucursal(idSucursal);
-        if(sucursal!=null){
+        if (sucursal != null) {
             resultado[0] = sucursal.getCapacidad();
             resultado[1] = sucursal.getPersonasDentro();
             return resultado;
@@ -46,33 +46,61 @@ public class SurcursalWebServices {
 
     @WebMethod
     public Object[] agregarTest(DtoTesting dtoTesting) {
-        Object[] resultado = new Object[2];
+        Object[] resultado = new Object[3];
         Boolean estatus = false;
+        String error = "";
         int cantPersonadentro = -1;
         Modulo aux = controladora.buscarModulo(dtoTesting.getIdModulo());
         if (dtoTesting.isTipoModulo()) {
-            // testing normal
-            estatus = controladora.agregarTesting(new TestingNormal(dtoTesting.isMascarilla(), dtoTesting.getTemperatura(), dtoTesting.getFechaResgistro(), (ModuloNormal) aux));
+            /**
+             *  TESTING NORMAL.
+             */
+
+            if (aux != null) {
+                if (aux instanceof ModuloNormal) {
+                    estatus = controladora.agregarTesting(new TestingNormal(dtoTesting.isMascarilla(), dtoTesting.getTemperatura(), dtoTesting.getFechaResgistro(), (ModuloNormal) aux));
+                } else {
+                    error = "[Error] El tipo de modulo y el tipo de test no coinciden.";
+                    error += "\n El Modulo y el Test deben ser Normales";
+                }
+            } else {
+                error = "[Info] No se encontro el modulo con el ID: " + dtoTesting.getIdModulo();
+            }
         } else {
-            // testing con prioridad
+            /**
+             *  testing con prioridad.
+              */
             DtoPersona dtoPersona = Controladora.getInstance().buscarPersonaDTO(dtoTesting.getCedulaPersona());
             if (dtoPersona != null) {
-                Persona persona = new Persona(dtoPersona.getCedula(), dtoPersona.getPrimerNombre(), dtoPersona.getSegundoNombre(), dtoPersona.getPrimerApellido(), dtoPersona.getSegundoApellido(), dtoPersona.getCorreo());
-               estatus = controladora.agregarTesting(new TestingPriority(dtoTesting.isMascarilla(), dtoTesting.getTemperatura(), dtoTesting.getFechaResgistro(), persona, (ModuloPrioridad) aux));
+                if (aux != null) {
+                    if (aux instanceof ModuloPrioridad) {
+                        Persona persona = new Persona(dtoPersona.getCedula(), dtoPersona.getPrimerNombre(), dtoPersona.getSegundoNombre(), dtoPersona.getPrimerApellido(), dtoPersona.getSegundoApellido(), dtoPersona.getCorreo());
+                        estatus = controladora.agregarTesting(new TestingPriority(dtoTesting.isMascarilla(), dtoTesting.getTemperatura(), dtoTesting.getFechaResgistro(), persona, (ModuloPrioridad) aux));
+                    } else {
+                        error = "[Error] El tipo de modulo y el tipo de test no coinciden.";
+                        error += "\n El Modulo y el Test deben ser Prioritario.";
+                    }
+                } else {
+                    error = "[Info] No se encontro el modulo con el ID: " + dtoTesting.getIdModulo();
+                }
             }
         }
-        if(estatus){
+        if (estatus) {
             //cantidad de persona ++
             Sucursal sucursal = controladora.buscarSucursal(aux.getSucursal().getId());
-            if(sucursal!=null){
-                //actulizando sucursal (solo cantidad de persona dentro)
-                cantPersonadentro = sucursal.getPersonasDentro()+1;
-                sucursal.setPersonasDentro(cantPersonadentro);
-                controladora.actualizarSucursal(sucursal);
+            if (sucursal != null) {
+                cantPersonadentro = sucursal.getPersonasDentro();
+                if (dtoTesting.isEstatus()) {
+                    //actulizando sucursal (solo cantidad de persona dentro)
+                    cantPersonadentro = sucursal.getPersonasDentro() + 1;
+                    sucursal.setPersonasDentro(cantPersonadentro);
+                    controladora.actualizarSucursal(sucursal);
+                }
             }
         }
         resultado[0] = estatus;
         resultado[1] = cantPersonadentro;
+        resultado[2] = error;
         return resultado;
     }
 
@@ -80,8 +108,8 @@ public class SurcursalWebServices {
     public boolean salidadDePersona(String idSucursal) {
         //descontarPersona
         Sucursal sucursal = controladora.buscarSucursal(idSucursal);
-        if(sucursal!=null){
-            sucursal.setPersonasDentro(sucursal.getPersonasDentro()-1);
+        if (sucursal != null) {
+            sucursal.setPersonasDentro(sucursal.getPersonasDentro() - 1);
             controladora.actualizarSucursal(sucursal);
             return true;
         }
